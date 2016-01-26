9b613579e4649e5cb7933f8839784010d93dc732
JayrAlencar <jayralencarpereira@gmail.com>
2016-01-26 18:58:01 -0300

    Comentários de descrição

--- BuscaBR.class.php
+++ BuscaBR.class.php
@@ -1,6 +1,30 @@
 <?php
+
+/**
+ * BuscaBR - Biblioteca PHP para algoritmo de busca fonética BuscaBR.
+ * @version 0.1.0
+ * @package BuscaBR
+ * @link https://github.com/ClubeDosGeeksCoding/buscabr-php GitHub project
+ * @author Jayr Alencar (jayralencar) <jayralencarpereira@gmail.com>
+ * @copyright 2016 - Clube dos Geeks (http://clubedosgeeks.com.br)
+ * @license The MIT License (MIT) (https://github.com/ClubeDosGeeksCoding/buscabr-php/blob/master/LICENSE)
+ **/
+
+
+
+/**
+ * BuscaBR - Biblioteca PHP para algoritmo de busca fonética BuscaBR.
+ * @package BuscaBR
+ * @author Jayr Alencar (jayralencar) <jayralencarpereira@gmail.com>
+ */
 class BuscaBR
 {
+	 /**
+     * encode texto para buscaBR.
+     * @param string $str
+     * @return string
+     * @author Jayr Alencar (jayralencar) <jayralencarpereira@gmail.com>
+     */
 	public static function encode($str){
 		$str = self::removeAcents($str);
 		$str = strtoupper($str);
@@ -32,10 +56,23 @@ class BuscaBR
 		return $str;
 	}
 
+	/**
+     * Remove acentos de string.
+     * @param string $string
+     * @return string
+     * @author Jayr Alencar (jayralencar) <jayralencarpereira@gmail.com>
+     */
+
 	private  function removeAcents($string){
 	    return preg_replace(array("/(á|à|ã|â|ä)/","/(Á|À|Ã|Â|Ä)/","/(é|è|ê|ë)/","/(É|È|Ê|Ë)/","/(í|ì|î|ï)/","/(Í|Ì|Î|Ï)/","/(ó|ò|õ|ô|ö)/","/(Ó|Ò|Õ|Ô|Ö)/","/(ú|ù|û|ü)/","/(Ú|Ù|Û|Ü)/","/(ñ)/","/(Ñ)/"),explode(" ","a A e E i I o O u U n N"),$string);
 	}
 
+	/**
+     * Remove caracteres duplicados seguidos.
+     * @param string $str
+     * @return string
+     * @author Jayr Alencar (jayralencar) <jayralencarpereira@gmail.com>
+     */
 	private function squeeze($str){
 		$cont = 0;
 		$strArray = str_split($str);
